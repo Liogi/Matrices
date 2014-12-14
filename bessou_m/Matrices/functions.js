c0f9e6ce18150c085483995bdf3d61acc49ab824
Liogi <lionel.giberne@gmail.com>
2014-12-14 15:47:13 +0100

    agestion d'erreur quand les champs de somme.html sont mal saisis

--- bessou_m/Matrices/functions.js
+++ bessou_m/Matrices/functions.js
@@ -2,7 +2,44 @@
 $(':checkbox').click(function() {
 	$(':checkbox:not(:focus)').attr('checked', false);
 	var str = this.id.split('_');
+	// ajax : ajout du champ consacré selon l'opération choisie
 	$('#operand').load(str[1]+'.html', function() {
 		alert("Vous avez choisi l'opération "+str[1]);
-	})
+		// action après click sur valider
+		action_performed();
+	});
 });
+
+function action_performed()
+{
+	var bool = true;
+	$('#buttonSum').click(function() {
+		$(':text').each(function () {
+			if ($(this).val() == false){
+				alert('Tous les champs doivent être renseignés');
+				bool = false;
+				return bool;
+			}
+		});
+		if (bool == false)
+			return false;
+		$(':text').each(function() {
+			var nb = $(this).val();
+			if (is_int(nb) == false){
+				alert('Les champs doivent être des nombres entiers');
+				bool = false;
+				return bool;
+			}
+		});
+		if (bool == false)
+			return false;
+	});
+}
+
+function is_int(val)
+{
+	if((parseFloat(val) === parseInt(val)) && !isNaN(val))
+		return true;
+	else
+		return false;
+}
