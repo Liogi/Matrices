a5ab2ec33e69c6c4b0a49e9f64ca85ab9dea8494
Liogi <lionel.giberne@gmail.com>
2014-12-14 13:36:53 +0100

    Creation de style.css et de somme.html pour l'entree formualaire, integration de somme.html dans form.html avec du ajax sous forme jquery

--- bessou_m/Matrices/functions.js
+++ bessou_m/Matrices/functions.js
@@ -1,4 +1,8 @@
 /** Radio perform with Checkbox**/
 $(':checkbox').click(function() {
 	$(':checkbox:not(:focus)').attr('checked', false);
+	var str = this.id.split('_');
+	$('#operand').load(str[1]+'.html', function() {
+		alert("Vous avez choisi l'opération "+str[1]);
+	})
 });
--- bessou_m/Matrices/somme.php
+++ bessou_m/Matrices/somme.php
@@ -1,1 +1,12 @@
 <?php
+
+echo "<h3> Calcul de Somme matriciel</h3>";
+echo "Nombre de lignes de la matrice A : ";
+echo "<input type='text' name='ligneA' id='ligneA' size='3' /></br>";
+echo "Nombre de colonnes de la matrice A : ";
+echo "<input type='text' name='colA' id='colA' size='3' /></br>";
+echo "Nombre de lignes de la matrice B : ";
+echo "<input type='text' name='ligneB' id='ligneB' size='3' /></br>";
+echo "Nombre de colonnes de la matrice B : ";
+echo "<input type='text' name='colB' id='colB' size='3' /></br>";
+echo ""
